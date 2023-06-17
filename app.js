7199e188640e4c932af44352ff7275b6a30074e7
Ashish Bhargav <ashishbhargav11072003@gmail.com>
2023-06-17 13:03:37 +0530

    dashboard commit

--- app.js
+++ app.js
@@ -5,13 +5,10 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session')
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 var db = require('./database');
-// var flash = require('express-flash')
 
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
@@ -28,33 +25,23 @@ app.use(session({
   resave: true,
   saveUninitialized: true
 }));
-// app.use(flash())
 
 app.use('/', indexRouter);
 
-// catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('');
 });
-
-
 
 app.listen(5000, () => {
   console.log('Listening on port 5000...');
 });
-
-
-
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -4,31 +4,14 @@ var mysql = require('mysql');
 var bcrypt = require('bcrypt');
 var con = require('../database');
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
-  if (req.session.flag == 1) {
-    req.session.destroy();
-    res.render('index', { title: 'scaleedge', message: 'Email Already Exists', flag: 1 });
-  }
-  else if (req.session.flag == 2) {
-    req.session.destroy();
-    res.render('index', { title: 'scaleedge', message: 'Registration Done. Please Login.', flag: 0 });
-  }
-  else if (req.session.flag == 3) {
-    req.session.destroy();
-    res.render('index', { title: 'scaleedge', message: 'Confirm Password Does Not Match.', flag: 1 });
-  }
-  else if (req.session.flag == 4) {
-    req.session.destroy();
-    res.render('index', { title: 'scaleedge', message: 'Incorrect Email or Password.', flag: 1 });
-  }
-  else {
-    res.render('index', { title: 'scaleedge' });
-  }
-
+  res.render('login', { title: 'scaleedge' });
 });
 
-//Handle POST request for User Registration
+router.get('/auth_reg', function (req, res, next) {
+  res.render('register', { title: 'scaleedge' });
+});
+
 router.post('/auth_reg', function (req, res, next) {
   var company_name = req.body.company_name;
   var address = req.body.address;
@@ -37,34 +20,37 @@ router.post('/auth_reg', function (req, res, next) {
   var password = req.body.password;
   var cpassword = req.body.cpassword;
   var name = req.body.name;
+
   if (cpassword == password) {
-    var sql = 'select * from user_master where email = ?;';
-    con.query(sql, [email], function (err, result, fields) {
+    var insertSql = 'CALL insert_company_user (?, ?, ?, ?, ?);';
+    con.query(insertSql, [company_name, address, email, mobile, password, name], function (err, result, fields) {
       if (err) throw err;
 
-      if (result.length > 0) {
-        req.session.flag = 1;
+      if (result.affectedRows > 0) {
         res.redirect('/');
       } else {
-        // var hashpassword = bcrypt.hashSync(password, 10);
-        //var sql = 'insert into user(company_name,email,password, name , address , mobile) values(?,?,?, ?, ?,?);';
-        var sql = 'CALL insert_company_user (?,?,?,?,?);';
-        //con.query(sql,[company_name,email, hashpassword, name , address , mobile], function(err, result, fields){
-        con.query(sql, [company_name, address, email, mobile, password, name], function (err, result, fields) {
-          if (err) throw err;
-          req.session.flag = 2;
-          res.redirect('/');
-        });
+        res.redirect('/');
       }
     });
   } else {
-    req.session.flag = 3;
     res.redirect('/');
   }
 });
 
 
-// Handle POST request for User Login
+
+router.get('/auth_login', (req, res) => {
+  con.query('SELECT * FROM user_master', (err, rows) => {
+    if (!err) {
+      let removedUser = req.query.removed;
+      res.render('dashboard', { rows, removedUser });
+    } else {
+      console.log(err);
+    }
+    console.log('The data from user table: \n', rows);
+  });
+});
+
 router.post('/auth_login', function (req, res, next) {
   var email = req.body.email;
   var password = req.body.password;
@@ -78,37 +64,19 @@ router.post('/auth_login', function (req, res, next) {
 
     if (message === 'Login successful.') {
       req.session.email = email;
-      res.redirect('/home');
+      res.send({ success: true });
     } else {
-      req.session.flag = 4;
-      res.redirect('/');
+      res.send({ success: false, message: 'Invalid email or password.' });
     }
-  });
-});
-
-
-
-router.get('/home', function (req, res, next) {
-  // res.render('home', { message: 'Welcome, ' + req.session.email });
-  con.query('SELECT * FROM user_master', (err, rows) => {
-    if (!err) {
-      let removedUser = req.query.removed;
-      res.render('home', { rows, removedUser });
-    } else {
-      console.log(err);
-    }
-    console.log('The data from user table: \n', rows);
   });
 });
 
 router.get('/logout', function (req, res, next) {
   if (req.session.email) {
     req.session.destroy();
-    res.redirect('/');
   }
-})
-
-
+  res.redirect('/');
+});
 
 router.get('/add', function (req, res, next) {
   res.render('adduser', { message: 'Welcome, ' + req.session.email });
@@ -116,6 +84,10 @@ router.get('/add', function (req, res, next) {
 
 router.get('/edit', function (req, res, next) {
   res.render('edit', { message: 'Welcome, ' + req.session.email });
+});
+
+router.get('/register', function (req, res, next) {
+  res.render('error', { message: 'Welcome, ' + req.session.email });
 });
 
 module.exports = router;
