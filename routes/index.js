d84ccd7f8218faa5fdbefb895c7e28b6fe562aae
Ashish Bhargav <ashishbhargav11072003@gmail.com>
2023-06-19 19:35:29 +0530

    add user

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,21 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var mysql = require('mysql');
-var bcrypt = require('bcrypt');
-var con = require('../database');
+var mysql = require("mysql");
+var bcrypt = require("bcrypt");
+var con = require("../database");
+const fileUpload = require('express-fileupload');
+router.use(fileUpload());
 
-router.get('/', function (req, res, next) {
-  res.render('login', { title: 'scaleedge' });
+
+router.get("/", function (req, res, next) {
+  res.render("login", { title: "scaleedge" });
 });
 
-router.get('/auth_reg', function (req, res, next) {
-  res.render('register', { title: 'scaleedge' });
+router.get("/auth_reg", function (req, res, next) {
+  res.render("register", { title: "scaleedge" });
 });
 
-router.post('/auth_reg', function (req, res, next) {
+router.post("/auth_reg", function (req, res, next) {
   var company_name = req.body.company_name;
   var address = req.body.address;
   var email = req.body.email;
@@ -22,72 +25,119 @@ router.post('/auth_reg', function (req, res, next) {
   var name = req.body.name;
 
   if (cpassword == password) {
-    var insertSql = 'CALL insert_company_user (?, ?, ?, ?, ?);';
-    con.query(insertSql, [company_name, address, email, mobile, password, name], function (err, result, fields) {
-      if (err) throw err;
+    var insertSql = "CALL insert_company_user (?, ?, ?, ?, ?);";
+    con.query(
+      insertSql,
+      [company_name, address, email, mobile, password, name],
+      function (err, result, fields) {
+        if (err) throw err;
 
-      if (result.affectedRows > 0) {
-        res.redirect('/');
-      } else {
-        res.redirect('/');
+        if (result.affectedRows > 0) {
+          res.redirect("/");
+        } else {
+          res.redirect("/");
+        }
       }
-    });
+    );
   } else {
-    res.redirect('/');
+    res.redirect("/");
   }
 });
 
-
-
-router.get('/auth_login', (req, res) => {
-  con.query('SELECT * FROM user_master', (err, rows) => {
+router.get("/dashboard", (req, res) => {
+  con.query("CALL user_master_fetchdata()", (err, rows) => {
     if (!err) {
       let removedUser = req.query.removed;
-      res.render('dashboard', { rows, removedUser });
+      res.render("dashboard", { rows: rows[0], removedUser });
     } else {
       console.log(err);
     }
-    console.log('The data from user table: \n', rows);
+    console.log("The data from user table: \n", rows[0]);
   });
 });
 
-router.post('/auth_login', function (req, res, next) {
+
+
+router.post("/auth_login", function (req, res, next) {
   var email = req.body.email;
   var password = req.body.password;
 
-  var sql = 'CALL loginUser(?, ?);';
+  var sql = "CALL loginUser(?, ?);";
 
   con.query(sql, [email, password], function (err, result, fields) {
     if (err) throw err;
 
     var message = result[0][0].message;
 
-    if (message === 'Login successful.') {
+    if (message === "Login successful.") {
       req.session.email = email;
       res.send({ success: true });
     } else {
-      res.send({ success: false, message: 'Invalid email or password.' });
+      res.send({ success: false, message: "Invalid email or password." });
     }
   });
 });
 
-router.get('/logout', function (req, res, next) {
+router.get("/logout", function (req, res, next) {
   if (req.session.email) {
     req.session.destroy();
   }
-  res.redirect('/');
+  res.redirect("/");
 });
 
-router.get('/add', function (req, res, next) {
-  res.render('adduser', { message: 'Welcome, ' + req.session.email });
+router.get("/add", function (req, res, next) {
+  res.render("adduser", { message: "Welcome, " + req.session.email });
 });
 
-router.get('/edit', function (req, res, next) {
-  res.render('edit', { message: 'Welcome, ' + req.session.email });
+router.get("/edit", function (req, res, next) {
+  res.render("edit", { message: "Welcome, " + req.session.email });
 });
 
-// router.get('/register', function (req, res, next) {
-//   res.render('error', { message: 'Welcome, ' + req.session.email });
-// });
+router.get("/delete/:id", function (req, res, next) {
+  var userId = req.params.id;
+
+  var deleteSql = "CALL delete_data(?);";
+  con.query(deleteSql, [userId], function (err, result) {
+    if (err) throw err;
+
+    console.log("User deleted:", userId);
+    res.redirect("/dashboard?removed=true");
+  });
+});
+
+
+
+router.post("/add", function (req, res, next) {
+  var user_name = req.body.user_name;
+  var email = req.body.email;
+  var mobile = req.body.mobile;
+  var password = req.body.password;
+  var profileImage = req.files.Imagepath;
+  var imagePath = "public/images/" + profileImage.name;
+
+  var insertSql = "CALL user_master_userformdata (?,?,?,?,?)";
+
+  con.query(insertSql, [user_name, email, mobile, password, imagePath], function (err, result, fields) {
+    if (err) {
+      console.error("An error occurred while adding the user:", err);
+      res.redirect("/dashboard");
+    } else {
+      profileImage.mv(imagePath, function (err) {
+        if (err) {
+          console.error("Failed to save profile image:", err);
+        }
+      });
+
+      if (result.affectedRows > 0) {
+        console.log("User added successfully.");
+        res.redirect("/dashboard");
+      } else {
+        res.redirect("/dashboard");
+      }
+    }
+  });
+});
+
+
 
 module.exports = router;
